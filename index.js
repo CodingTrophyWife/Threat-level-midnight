1c3989645c4d6f037ea086e12b370857b58a75c7
Cyndi Fosmark <cmfosmark@gmail.com>
2023-06-07 16:28:05 -0700

    updates

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = 3003;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log('Threat level midnight now on  http://localhost:' + PORT));
+  });
